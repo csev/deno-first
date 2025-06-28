2ab19c5ffb8687bcd26fcd663c64e888c75cf58d
Charles Severance <csev@umich.edu>
2025-06-28 13:53:22 -0400

    Add list feature

--- main.ts
+++ main.ts
@@ -3,15 +3,29 @@ import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
 const app = new Hono();
 const kv = await Deno.openKv();
 
-// Get a book by title
+// Get a record by key
 app.get("/kv/get/:key{.*}", async (c) => {
   const key = c.req.param("key");
   const result = await kv.get(key.split('/'));
   return c.json(result);
 });
 
+// List records with a key prefix
+app.get("/kv/list/:key{.*}", async (c) => {
+  const key = c.req.param("key");
+  const iter = await kv.get({ prefix: key.split('/') });
+  const records = [];
+  for await (const entry of iter) {
+    records.push(entry);
+    console.log(entry.key);
+    console.log(entry.value);
+  }
+  console.log('iter', iter.cursor);
+  return c.json({'records': records, 'cursor': iter.cursor});
+});
 
-// Create a book (POST body is JSON)
+
+// Set a record by key (POST body is JSON)
 app.post("/kv/set/:key{.*}", async (c) => {
   const key = c.req.param("key");
   const body = await c.req.json();
